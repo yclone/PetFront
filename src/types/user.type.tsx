718498a3c7b91664444d2b525fc6e05ce4069f7b
Qa com Marra <vinicius_marra@hotmail.com>
2023-11-26 10:36:22 -0300

    rem projeto antigo

--- src/types/user.type.tsx
+++
@@ -1,9 +0,0 @@
-export default interface IUser {
-  id?: any | null,
-  username?: string | null,
-  nome?: string | null,
-  sobrenome?: string | null,
-  email?: string,
-  password?: string,
-  roles?: Array<string>
-}
